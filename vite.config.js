fb6d18d81cc1da767b0ddb202a721a119ffc1aa5
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2024-09-15 21:58:17 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ htmlFiles.forEach((file) => {
 });
 
 export default defineConfig({
-  base: '/vite-landing-template',
+  base: '/GronisLanding',
   root: 'src',
   publicDir: '../public',
   plugins: [
